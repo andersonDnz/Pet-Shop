69340ea11eeb97c70db860eb02f84dfffd5963d1
Anderson Diniz <andersondinizep@gmail.com>
2023-03-31 20:38:14 -0300

    Lidando com possíveis erros

--- clientes/controller/atualizaCliente-controller.js
+++ clientes/controller/atualizaCliente-controller.js
@@ -9,17 +9,30 @@ import { clienteService } from "../service/cliente-service.js";
 
   const inputEmail = document.querySelector("[data-email]");
 
-  const dados = await clienteService.detalhaCliente(id);
+  try {
+    const dados = await clienteService.detalhaCliente(id);
   inputNome.value = dados.nome;
   inputEmail.value = dados.email;
+  } catch (erro) {
+    console.log(erro)
+    window.location.href="../telas/erro.html"
+  }
+  
   
   const formulario = document.querySelector("[data-form]");
 
   formulario.addEventListener("submit", async (evento) => {
     evento.preventDefault();
 
-    await clienteService.atualizacliente(id, inputNome.value, inputEmail.value);
+    try {
+      await clienteService.atualizacliente(id, inputNome.value, inputEmail.value);
 
     window.location.href = "../telas/edicao_concluida.html";
+    } catch (erro) {
+      console.log(erro)
+      window.location.href="../telas/erro.html"
+    }
+
+    
   });
 })();
--- clientes/service/cliente-service.js
+++ clientes/service/cliente-service.js
@@ -1,6 +1,9 @@
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`).then((resposta) => {
-    return resposta.json();
+    if (resposta.ok) {
+      return resposta.json();
+    }
+    throw new Error("Não foi possivel listar os clientes");
   });
 };
 
@@ -15,19 +18,29 @@ const criaCliente = (nome, email) => {
       email: email,
     }),
   }).then((resposta) => {
-    return resposta.body;
+    if (resposta.ok) {
+      return resposta.body;
+    }
+    throw new Error("Não foi possivel criar um cliente");
   });
 };
 
 const removeCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`, {
     method: "DELETE",
+  }).then((resposta) => {
+    if (!resposta.ok) {
+      throw new Error("Não foi possivel remover um cliente");
+    }
   });
 };
 
 const detalhaCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`).then((resposta) => {
-    return resposta.json();
+    if (resposta.ok) {
+      return resposta.json();
+    }
+    throw new Error("Não foi possivel detalhar o cliente");
   });
 };
 
@@ -42,7 +55,10 @@ const atualizacliente = (id, nome, email) => {
       email: email,
     }),
   }).then((resposta) => {
-    return resposta.json();
+    if (resposta.ok) {
+      return resposta.json();
+    }
+    throw new Error("Não foi possivel atualizar um clientes");
   });
 };
 
